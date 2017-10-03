3e97f9b7766b94d3218c8e09ecc1a4c802859365
Wolfgang Fahl <wf@bitplan.com>
2017-10-03 16:45:46 +0200

    adds Java installation check

--- specials/SpecialCMSFrontend.php
+++ specials/SpecialCMSFrontend.php
@@ -18,10 +18,51 @@ class SpecialCMSFrontend extends SpecialPage {
 	public function execute( $sub ) {
 		$out = $this->getOutput();
 		$out->setPageTitle( $this->msg( 'special-cMSFrontend-title' ) );
+		// FIXME - should be help for this extension
 		$out->addHelpLink( 'How to become a MediaWiki hacker' );
 		$out->addWikiMsg( 'special-cMSFrontend-intro' );
+  		$this->getJavaVersion();
+		
 	}
 
+	/**
+	 * exec a command and return stdout and stderr
+	 * https://stackoverflow.com/a/25879953/1497139
+	 */
+	function exec($cmd, &$stdout=null, &$stderr=null) {
+	    $proc = proc_open($cmd,[
+		1 => ['pipe','w'],
+		2 => ['pipe','w'],
+	    ],$pipes);
+	    $stdout = stream_get_contents($pipes[1]);
+	    fclose($pipes[1]);
+	    $stderr = stream_get_contents($pipes[2]);
+	    fclose($pipes[2]);
+	    return proc_close($proc);
+	}
+
+	/**
+         * get the java version that is installed on this computer
+         *
+         */
+	public function getJavaVersion() {
+ 		$cmd = "java -version";
+		$return_code = $this->exec($cmd,$output,$err);
+		$out = $this->getOutput();
+   		// if command could not be executed
+		if ($return_code == 127) {
+			// java is not installed or not in path
+			// FIXME i18n
+			$out->addWikiText( 'Java is not installed or configured.' );
+		} else {
+			$out->addWikiText('https://upload.wikimedia.org/wikipedia/de/thumb/e/e1/Java-Logo.svg/75px-Java-Logo.svg.png is installed ✓');
+			$out->addWikiText( '<pre>'.$err.'</pre>');
+		}
+ 	}
+
+	/**
+	 * get the group this special page belongs to
+	 */ 
 	protected function getGroupName() {
 		return 'other';
 	}
